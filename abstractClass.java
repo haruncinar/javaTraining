426f3ebe525b814c93e39772627cb18a85ef2372
Harun Çınar <55881311+haruncinar@users.noreply.github.com>
2020-01-31 00:03:54 +0300

    Update abstractClass.java

--- abstractClass.java
+++ abstractClass.java
@@ -1,5 +1,5 @@
 /*
-A Java abstract class is a class that can't be instantiated. That means you cannot create new instances of an abstract class. It works as a base for subclasses. You should learn about Java Inheritance before attempting this challenge.
+A Java abstract class is a class that can't be instantiated. That means you cannot create new instances of an abstract class.. It works as a base for subclasses. You should learn about Java Inheritance before attempting this challenge.
 
 Following is an example of abstract class:
 
